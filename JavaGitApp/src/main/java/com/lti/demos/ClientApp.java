3900f12f84613d267e9b6930e1fe0d6bb7329fa7
ChandanKumarDS <chandankumards1999@gmail.com>
2021-12-01 14:53:39 +0530

    Hi hello

---
+++ JavaGitApp/src/main/java/com/lti/demos/ClientApp.java
@@ -0,0 +1,13 @@
+package com.lti.demos;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			Product p1 = new Product(10, "Hellp");
+			System.out.println(p1);
+		}
+	}
+
+
